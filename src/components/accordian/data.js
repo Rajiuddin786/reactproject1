246bb13cfe6ab5198af72523557e9cd61e1e9f6a
Rajiuddin786 <skrajiuddin18@gmail.com>
2026-08-14 09:28:00 +0530

    initial commit

---
+++ src/components/accordian/data.js
@@ -0,0 +1,19 @@
+const data = [
+    {
+        id:'1',
+        question:'What is the purpose of this application?',
+        answer:'The purpose of this application is to provide users with a platform to manage their tasks efficiently and effectively.'
+    },
+    {
+        id:'2',
+        question:'How can I create a new task?',
+        answer:'To create a new task, click on the "Add Task" button and fill in the required details such as task name, description, and due date.'
+    },
+    {
+        id:'3',
+        question:'Can I set reminders for my tasks?',
+        answer:'Yes, you can set reminders for your tasks by selecting the reminder option when creating or editing a task.'
+    }
+]
+
+export default data;
